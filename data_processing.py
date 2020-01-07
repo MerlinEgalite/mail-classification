dbfa487890eefdeb9f87dc0b1e7a259b2fb9e53c
Merlin Egalite <merlin.egalite@student-cs.fr>
2020-01-07 14:11:51 +0100

    added some processing

--- data_processing.py
+++ data_processing.py
@@ -26,8 +26,12 @@ new_test_df = test_df.copy()
 new_test_df = new_test_df.drop(['date', 'bcced'], axis=1)
 
 
+## Function for processing data
 def processing(dataset, new_dataset):
+
     for (index, row) in dataset['date'].items():
+
+        # Process for date
         date_list = row.split(' ')
         if len(date_list) > 4:
             day = date_list[0][0:3]
@@ -35,14 +39,19 @@ def processing(dataset, new_dataset):
             hour = float(date_list[4][0:2])
             minutes = float(date_list[4][3:5]) / 60
             new_hour = hour + minutes
-            print(day, month, new_hour)
             new_dataset.at[index, 'day'] = day
             new_dataset.at[index, 'month'] = month
             new_dataset.at[index, 'hour'] = new_hour
+
+        # Process for mail_type
+        new_dataset.at[index, 'mail_type'] = new_dataset.at[index, 'mail_type'].lower()
+        new_dataset.at[index, 'org'] = new_dataset.at[index, 'org'].lower()
+        new_dataset.at[index, 'tld'] = new_dataset.at[index, 'tld'].lower()
+
     return new_dataset
 
 
-## Modify date column
+## Modify the Train & Test sets
 new_train_df = processing(train_df, new_train_df)
 new_test_df = processing(test_df, new_test_df)
 
@@ -50,22 +59,3 @@ new_test_df = processing(test_df, new_test_df)
 ## Save into csv files
 new_train_df.to_csv("new_train.csv", index=True, index_label='Id')
 new_test_df.to_csv("new_test.csv", index=True, index_label='Id')
-
-
-"""for (index, row) in train_df['date'].items():
-    date_list = row.split(' ')
-    if len(date_list) == 6:
-        day = date_list[0][0:3]
-        month = date_list[2]
-        hour = float(date_list[4][0:2])
-        minutes = float(date_list[4][3:5]) / 60
-        new_hour = hour + minutes
-        print(new_hour)
-        new_train_df.at[index, 'day'] = day
-        new_train_df.at[index, 'month'] = month
-        new_train_df.at[index, 'hour'] = new_hour"""
-
-
-
-
-
