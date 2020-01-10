99bf7d65a141da1303900509f154b38ebdf55d50
Merlin Egalite <merlin.egalite@student-cs.fr>
2020-01-10 22:07:30 +0100

    improve data preprocessing and improve xgboost

--- data_processing.py
+++ data_processing.py
@@ -1,14 +1,17 @@
-"""
-This script can be used as skeleton code to read the challenge train and test
-"""
 import pandas as pd
 import re
+import numpy as np
+import missingno as msno
 
 from sklearn.preprocessing import LabelEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+months = {'JAN':'Jan', 'FEV':'Feb', 'MAR':'Mar', 'AVR':'Apr', 'MAI':'May', 'JUN':'Jun', 'JUI':'Jul', 'AOU':'Aug', 'SEP':'Sep', 'OCT':'Oct', 'NOV':'Nov', 'DEC':'Dec'}
 
 # Read csv
-train_df = pd.read_csv('train.csv', index_col=0)
-test_df = pd.read_csv('test.csv', index_col=0)
+train_df = pd.read_csv('datasets/train.csv', index_col=0)
+test_df = pd.read_csv('datasets/test.csv', index_col=0)
 
 # Handle missing values
 train_df.fillna('NA', inplace=True)
@@ -35,13 +38,19 @@ def processing(dataset, new_dataset):
 
     for (index, row) in dataset['date'].items():
 
+        date_list = row.split(' ')
+
+        # Prevent empty string
+        if '' in date_list:
+            date_list.remove('')
+
         # Process for date column
         # Regex expression to match the right date expression
-        prog = re.compile('''(\")?([a-zA-Z]{3}), +([0-9]{1,2}) ([a-zA-Z]{3}) ([0-9]{4}) ([0-9:]{8})''')
+        prog = re.compile('''(\")?([a-zA-Z]{3}), +([0-9]{1,2}) +([a-zA-Z]{3}) +([0-9]{4}) +([0-9:]{8})''')
         result = prog.match(row)
+
         if result is not None:
             # Get the day, date, month and create new columns
-            date_list = row.split(' ')
             day = date_list[0][0:3]
             month = date_list[2]
             hour = float(date_list[4][0:2])
@@ -51,13 +60,38 @@ def processing(dataset, new_dataset):
             new_dataset.at[index, 'month'] = month
             new_dataset.at[index, 'hour'] = new_hour
 
+        else:
+
+            prog = re.compile('''([0-9]{1,2}) +([a-zA-Z]{3}) +([0-9]{4}) +([0-9:]{8})''')
+            result = prog.match(row)
+
+            if result is not None:
+
+                hour = float(date_list[3][0:2])
+                minutes = float(date_list[3][3:5]) / 60
+                new_hour = hour + minutes
+                new_dataset.at[index, 'day'] = ""
+                new_dataset.at[index, 'month'] = month
+                new_dataset.at[index, 'hour'] = new_hour
+
+            else:
+
+                month = months[date_list[0][3:6]]
+                hour = float(date_list[1][0:2])
+                minutes = float(date_list[1][3:5]) / 60
+                new_hour = hour + minutes
+                new_dataset.at[index, 'day'] = ""
+                new_dataset.at[index, 'month'] = month
+                new_dataset.at[index, 'hour'] = new_hour
+
+
         # Process for mail_type
         new_dataset.at[index, 'mail_type'] = new_dataset.at[index, 'mail_type'].lower()
         new_dataset.at[index, 'org'] = new_dataset.at[index, 'org'].lower()
         new_dataset.at[index, 'tld'] = new_dataset.at[index, 'tld'].lower()
 
     # Label Encoder to transform categories into numbers
-    categorical = list(new_dataset)
+    categorical = ['day', 'month', 'org', 'tld', 'mail_type']
     for cat in categorical:
         new_dataset[cat] = LabelEnc(new_dataset, cat)
 
@@ -68,6 +102,13 @@ def processing(dataset, new_dataset):
 new_train_df = processing(train_df, new_train_df)
 new_test_df = processing(test_df, new_test_df)
 
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+new_train_df[['org', 'hour', 'day', 'month', 'salutations']] = imputer.fit_transform(new_train_df[['org', 'hour', 'day', 'month', 'salutations']])
+
+
+msno.matrix(new_train_df)
+
 # Save into csv files
-new_train_df.to_csv("new_train.csv", index=True, index_label='Id')
-new_test_df.to_csv("new_test.csv", index=True, index_label='Id')
+new_train_df.to_csv("datasets/new_train.csv", index=True, index_label='Id')
+new_test_df.to_csv("datasets/new_test.csv", index=True, index_label='Id')
