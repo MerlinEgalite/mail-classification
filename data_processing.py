77a58b0c84669ba796fc96455e3023f43c2cc766
Merlin Egalite <merlin.egalite@student-cs.fr>
2020-01-10 17:28:26 +0100

    added label encoding to data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -2,22 +2,17 @@
 This script can be used as skeleton code to read the challenge train and test
 """
 import pandas as pd
+import re
 
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
 
-from sklearn.metrics import accuracy_score
-
-## Read csvs
+# Read csv
 train_df = pd.read_csv('train.csv', index_col=0)
 test_df = pd.read_csv('test.csv', index_col=0)
 
-## Handle missing values
+# Handle missing values
 train_df.fillna('NA', inplace=True)
 test_df.fillna('NA', inplace=True)
-
-days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 new_train_df = train_df.copy()
 new_train_df = new_train_df.drop(['date', 'bcced'], axis=1)
@@ -25,15 +20,28 @@ new_train_df = new_train_df.drop(['date', 'bcced'], axis=1)
 new_test_df = test_df.copy()
 new_test_df = new_test_df.drop(['date', 'bcced'], axis=1)
 
+# Label Encoder
+def LabelEnc(data_df, title_column):
+    """Return a new pandas dataframe with label encoder"""
+    lab_enc = LabelEncoder()
+    new_data_df = data_df.copy()
+    new = lab_enc.fit_transform(new_data_df[title_column].astype(str))
+    new_data_df[title_column] = new
+    return new_data_df[title_column]
 
-## Function for processing data
+
+# Function for processing data
 def processing(dataset, new_dataset):
 
     for (index, row) in dataset['date'].items():
 
-        # Process for date
-        date_list = row.split(' ')
-        if len(date_list) > 4:
+        # Process for date column
+        # Regex expression to match the right date expression
+        prog = re.compile('''(\")?([a-zA-Z]{3}), +([0-9]{1,2}) ([a-zA-Z]{3}) ([0-9]{4}) ([0-9:]{8})''')
+        result = prog.match(row)
+        if result is not None:
+            # Get the day, date, month and create new columns
+            date_list = row.split(' ')
             day = date_list[0][0:3]
             month = date_list[2]
             hour = float(date_list[4][0:2])
@@ -48,14 +56,18 @@ def processing(dataset, new_dataset):
         new_dataset.at[index, 'org'] = new_dataset.at[index, 'org'].lower()
         new_dataset.at[index, 'tld'] = new_dataset.at[index, 'tld'].lower()
 
+    # Label Encoder to transform categories into numbers
+    categorical = list(new_dataset)
+    for cat in categorical:
+        new_dataset[cat] = LabelEnc(new_dataset, cat)
+
     return new_dataset
 
 
-## Modify the Train & Test sets
+# Modify the Train & Test sets
 new_train_df = processing(train_df, new_train_df)
 new_test_df = processing(test_df, new_test_df)
 
-
-## Save into csv files
+# Save into csv files
 new_train_df.to_csv("new_train.csv", index=True, index_label='Id')
 new_test_df.to_csv("new_test.csv", index=True, index_label='Id')
